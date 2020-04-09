19de64cc459f24b729b10fd7cdb9f3bfd994711a
hanie <hanie.samimi@gmail.com>
2020-04-09 13:49:45 -0500

    Create: Main and read I/O

---
+++ k-mer.py
@@ -0,0 +1,44 @@
+# FILE: q2.py
+# AUTHOR: Hanie Samimi
+# CREATE DATE: 9 April 2020
+
+import sys
+import math
+
+def kmer(sequence):
+    print(sequence)
+
+def read_fasta(inputFile , k_size):
+    fasta_file = open(inputFile, "r")
+    counter = 1
+    sequence = ""
+    for line in fasta_file:
+        if line[0] is ">":
+            # call the kmer if sequence is not empty
+            if sequence is not "":
+                print('Analyzing chunk {} of data'.format(counter))
+                kmer(sequence)
+                sequence = sequence[-k_size+1:]
+                counter += 1
+        else:
+            if line[-1] is '\n':
+                line = line[:-1]
+            sequence = sequence + line
+
+
+## Main ##
+# GET THE FILE NAME FROM USER
+print("Enter K value:")
+k_size = int(input())
+
+inputFile: str = "/home/hanie/Desktop/IDbyDNACodeChallenge/SRR1748776.FASTA"
+print("Enter input file or skip [S] to use the default:")
+response: str = input()
+if response != "S":
+    inputFile = response
+
+# CHECK IF INPUT IS VALID
+
+# READ THE INPUT FILE
+read_fasta(inputFile, k_size)
+#read_fasta_sequence(inputFile)
